85f0db5f7c0aa37d6913fc4511a0bb8ded8d0ff0
mirontoli <mirontoli@gmail.com>
2011-04-18 23:07:26 +0200

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+# Schema version: 20110418202905
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
 end
