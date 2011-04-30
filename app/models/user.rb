7e6e858ebc0fb0c85197f341d1b962f573f33e06
mirontoli <mirontoli@gmail.com>
2011-04-30 14:23:19 +0200

    done with simple passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,31 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  :presence => true,
-                    :length   => { :maximum => 50 }
-  validates :email, :presence => true,
-                    :format   => { :with => email_regex },
-                    :uniqueness => { :case_sensitive => false }
+  validates :name,     :presence     => true,
+                       :length       => { :maximum => 50 }
+  validates :email,    :presence     => true,
+                       :format       => { :with => email_regex },
+                       :uniqueness   => { :case_sensitive => false }
+  # Automatically create the virtual attribute 'password_confirmation'
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temp implementation
+    end
 
 end
